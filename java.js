2dd292cef36e2baaa97757ac4f3ccf136760cd82
Ethan De Jesus <124072155+magicianZ@users.noreply.github.com>
2023-10-05 13:11:55 -0400

    e

--- java.js
+++ java.js
@@ -2,12 +2,12 @@ const dictionary = {
     name1:'ethan',
     teachers:['mrs.m','mr.michealwhale','mr.clengo','mr.m']
 }
-
-console.log(dictionary)
-
+teachers1 = ['mrs.m','mr.michealwhale','mr.clengo','mr.m']
 
 
-for (var i = 0;i <=2; i++){
+
+
+for (let i = 0;i = teachers1.length; i++){
     console.log(dictionary.teachers[i])
 
 }
