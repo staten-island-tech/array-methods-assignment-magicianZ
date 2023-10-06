7e4b24db196f7d5ca59fb5d1df76e99d129ce5cd
Ethan De Jesus <124072155+magicianZ@users.noreply.github.com>
2023-10-06 13:12:44 -0400

    thas crazy

--- java.js
+++ java.js
@@ -2,13 +2,34 @@ const dictionary = {
     name1:'ethan',
     teachers:['mrs.m','mr.michealwhale','mr.clengo','mr.m']
 }
-teachers1 = ['mrs.m','mr.michealwhale','mr.clengo','mr.m']
+//teachers1 = ['mrs.m','mr.michealwhale','mr.clengo','mr.m']
 
 
 
 
-for (let i = 0;i = teachers1.length; i++){
-    console.log(dictionary.teachers[i])
+//console.log(teachers1[teachers1.length - 1])
 
-}
+//teachers1.forEach((teacher)=>console.log(teacher))
+
+
+const student = [
+    {
+        name1:'e'
+    },
+    {
+        name1:'d'
+    },
+    {
+        name1:'f'
+    },
+
+]
+
+student.forEach((names)=>console.log(names.name1))
+
+
+
+
+ 
+
 
