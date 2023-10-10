5edc6091ec61deadd2bdc73737ca5230945910a5
Ethan De Jesus <124072155+magicianZ@users.noreply.github.com>
2023-10-10 13:17:17 -0400

    eeee

---
+++ noahrozin.js
@@ -0,0 +1,63 @@
+const dictionary = {
+    name1:'ethan',
+    teachers:['mrs.m','mr.michealwhale','mr.clengo','mr.m']
+}
+//teachers1 = ['mrs.m','mr.michealwhale','mr.clengo','mr.m']
+
+
+
+
+//console.log(teachers1[teachers1.length - 1])
+
+//teachers1.forEach((teacher)=>console.log(teacher))
+
+
+const student = [
+    {
+        name1:'e',
+        graduated:'nathan'
+
+    },
+    {
+        name1:'d',
+        graduated:'ethan'
+    },
+    {
+        name1:'f',
+        graduated:'truisten'
+    },
+
+]
+
+
+//student.forEach((i)=>console.log(i.name1))
+
+
+
+
+//student.forEach((i) => console.log(i.graduated))
+
+
+let variable = student.filter((i) => i.graduated.length < 5)
+
+
+
+console.log(variable)
+
+
+
+
+
+
+//const graduated = students.filter((student)=> student.graduated === trueor student.graduated)
+//console.log(graduated);
+
+
+
+
+
+
+
+ 
+
+
